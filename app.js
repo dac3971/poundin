5ff02be91585142a4a9e057aac1aed4988bb94aa
Taylor White <taylor.m.white@hotmail.com>
2019-03-18 19:01:34 -0400

    added /get/:id route

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require("express")
 const cheerio = require("cheerio")
 const request = require("request")
-const db = require("./config/database")
-const Puss = require("./models/Puss")
+const sequelize = require('sequelize')
 const url = require('url')
+const Op = sequelize.Op;
+const listing = require('./models').listing
+
 
 search_url = new URL("https://www.ebay.com/sch/i.html")
 search_url.searchParams.append('_udlo','50') //floor price
 search_url.searchParams.append('_sop','10') //newly listed
 search_url.searchParams.append('_nkw','textbook') //search term
 search_url.searchParams.append('_pgn','1') //page
-console.log(search_url)
+
 
 
 // request(completeURL, (error, response, html)=>{
@@ -24,20 +26,35 @@ console.log(search_url)
 // .catch(err => console.log("error:"+err))
 
 const app = express()
+app.get('/', (req,res) =>{
 
-app.get('/', (req,res)=> {
+
+})
+
+app.get('/get/:id', (req,res)=> {
     const data = {
-        id: "774",
+        listingID: "99",
         title: "coin 1",
         end: "tuesday",
         price: 1300,
         isbn: "IBNK"
     }
-    let { id,title,end,price,isbn } = data
+    let { listingID,title,end,price,isbn } = data
+    console.log(req.params.id)
+    listing.findByPk(req.params.id.toString()).then(item=>{
+        console.log(item.dataValues)
+        res.send(item.dataValues)
+    })
+    //Puss.create({id,title,end,price,isbn}).then(puss=>console.log('done')).catch(err=>console.log(err))
+    //listing.create({listingID,price}).then(listing=> console.log(listing)).catch(err=> console.log(err))
 
-    Puss.create({id,title,end,price,isbn}).then(puss=>console.log('done')).catch(err=>console.log(err))
+   
     
-    res.send(search_url)
+
+    
+    
+    
+    
 })
 const PORT = process.env.PORT || 5000
 
--- models/listing.js
+++ models/listing.js
@@ -1,7 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const listing = sequelize.define('listing', {
-    listingID: DataTypes.STRING
+    listingID: {
+        type: DataTypes.STRING,
+        primaryKey : true
+    },
+    title: DataTypes.STRING
   }, {});
   listing.associate = function(models) {
     // associations can be defined here
